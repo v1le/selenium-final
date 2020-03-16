0a7c335b9e6e9848d0a8c0deb9ca34986c30af7f
v1le <my@ema.il>
2020-03-16 18:39:37 +0300

    [*] changed method that checks login link to more readable format

--- test_main_page.py
+++ test_main_page.py
@@ -4,12 +4,14 @@ import time
 
 URL = "http://selenium1py.pythonanywhere.com/"
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
 
 def test_guest_can_go_to_login_page(browser):
-    main_page = MainPage(browser, URL)
-    main_page.open()
-    main_page.go_to_login_page()
+    page = MainPage(browser, URL)
+    page.open()
+    page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, URL)
+    page.open()
+    page.should_be_login_link()
