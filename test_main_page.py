eb40e04d3fcfbe2a7f7fe3d9043dfeb9895b586d
v1le <my@ema.il>
2020-03-16 17:39:07 +0300

    [+] basePage class

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,10 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+URL = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(URL)
+    go_to_login_page(browser)
