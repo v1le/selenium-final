7a1fa210dca29628b52a8749ca55d1d3e79c7c75
v1le <my@ema.il>
2020-03-16 18:13:06 +0300

    [*] modified test to POM

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,7 @@
+from pages.main_page import MainPage
+import time
+
+
 URL = "http://selenium1py.pythonanywhere.com/"
 
 def go_to_login_page(browser):
@@ -6,5 +10,6 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(URL)
-    go_to_login_page(browser)
+    main_page = MainPage(browser, URL)
+    main_page.open()
+    main_page.go_to_login_page()
