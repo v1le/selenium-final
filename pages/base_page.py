4baddb0accf0acc3f91515b5b94118c058ed9853
v1le <my@ema.il>
2020-03-17 20:09:07 +0300

    [*] moved login methods to basepage

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,8 @@ from selenium.common.exceptions import NoSuchElementException, NoAlertPresentExc
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
+from .locators import BasePageLocators
+
 
 class BasePage():
         def __init__(self, browser, url):
@@ -10,6 +12,13 @@ class BasePage():
 
         def open(self):
                 self.browser.get(self.url)
+
+        def go_to_login_page(self):
+                login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+                login_link.click()
+
+        def should_be_login_link(self):
+                assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
         def is_element_present(self, how, what):
                 try:
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,12 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import time
 
 URL = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+URL_mod = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
 
+@pytest.mark.skip
 @pytest.mark.parametrize('offers', [f'?promo=offer{offer}' for offer in range(10) if offer != 7] + [
     pytest.param('?promo=newYear2019', marks=pytest.mark.skip),
     pytest.param('?promo=offer7', marks=pytest.mark.xfail)
@@ -16,6 +19,7 @@ def test_guest_can_add_product_to_basket(browser, offers):
     page.are_prices_equal_on_success()
     page.are_names_equal_on_success()
 
+@pytest.mark.skip
 @pytest.mark.smoke
 def test_guest_cant_see_after_adding_product_to_basket(browser):
     page = ProductPage(browser, URL)
@@ -23,15 +27,29 @@ def test_guest_cant_see_after_adding_product_to_basket(browser):
     page.add_to_basket()
     page.should_not_be_success_message()
 
+@pytest.mark.skip
 @pytest.mark.smoke
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, URL)
     page.open()
     page.should_not_be_success_message()
 
+@pytest.mark.skip
 @pytest.mark.smoke
 def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, URL)
     page.open()
     page.add_to_basket()
     page.should_not_dissappear_success_alert()
+
+def test_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, URL_mod)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, URL_mod)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
